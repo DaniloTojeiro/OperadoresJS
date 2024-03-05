5aaaa7f2a7d7ee4c52a0d679513317a12092d18c
DaniloTojeiro <danilotojeiro@live.com>
2024-03-05 20:11:23 -0300

    Operadores

---
+++ resumo.js
@@ -0,0 +1,7 @@
+const nome = "Danilo";
+const idade = 30;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
---
+++ template-string.js
@@ -0,0 +1,10 @@
+const nome = "Danilo";
+const idade = 2023 - 1993;
+const cidadeDeNascimento = "Americana";
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+
+console.log (apresentacao) 
